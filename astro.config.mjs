115638f5cbb88bb732be0afdfc95f67e90ed78a6
Adam Bentley <bentleyadamj@gmail.com>
2025-09-08 12:53:31 +0100

    add header top bar and working mobile toggle

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
+import icon from 'astro-icon';
+
 export default defineConfig({
   site: "https://www.weekendtreesurgeon.netlify.app", // update me!
-  integrations: [
-    sitemap()
-  ]
+  integrations: [sitemap(), icon()]
 });
